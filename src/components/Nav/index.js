5999acc3a7d5b7d4739f20b4fbdbf823cb653614
Clinton <80900245+pravton@users.noreply.github.com>
2021-08-30 15:59:54 -0400

    made some chnages to mobile nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 function Nav({currentTheme, setTheme, setCurrentTheme }) {
+  const mobileMenuToggle = () => {
+    const navContainer = document.querySelector('.nav-container');
+    const navIcon = document.querySelector('#mobile-nav-icon');
+    if(navContainer.className.includes('nav-hide')) {
+      navContainer.classList.add('nav-show');
+      navIcon.classList.add('nav-open');
+      navContainer.classList.remove('nav-hide');
+    } else {
+      navContainer.classList.add('nav-hide');
+      navIcon.classList.remove('nav-open');
+      navContainer.classList.remove('nav-show');
+    }
+  }
+
   return (
     <header className="header">
       <div className="logo-container">
         <h1 className="logo-name">Clinton.</h1>
       </div>
-      <nav className="nav-container">
+      <div className="theme-switch-container">
+          <input onClick={() => setTheme(currentTheme, setCurrentTheme)} type="checkbox" id="toggle-switch" name="toggle-switch" className="toggle-input" />
+          <label htmlFor="toggle-switch" className="toggle-label">
+          </label>
+      </div>
+      <nav className="nav-container nav-hide">
         <ul className="nav">
           <li className="nav-item">
             <a href="#about">About Me</a>
@@ -21,12 +40,10 @@ function Nav({currentTheme, setTheme, setCurrentTheme }) {
             <a href="#resume">Resume</a>
           </li>
         </ul>
-        <div className="theme-switch-container">
-          <input onClick={() => setTheme(currentTheme, setCurrentTheme)} type="checkbox" id="toggle-switch" name="toggle-switch" className="toggle-input" />
-          <label htmlFor="toggle-switch" className="toggle-label">
-          </label>
-        </div>
       </nav>
+      <div id="mobile-nav-icon" className="mobile-nav-icon">
+        <div onClick={mobileMenuToggle} id="burgerBtn"></div>
+      </div>
     </header>
   )
 }
