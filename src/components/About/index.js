db2db4a88017677531230ad995c9cd8da7e76be8
Clinton <80900245+pravton@users.noreply.github.com>
2021-09-01 11:34:15 -0400

    created contacticons component and fixed css issues

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactIcons from '../ContactIcons';
 import aboutImg from '../../images/cover.jpg';
 
 function About() {
@@ -11,13 +12,7 @@ function About() {
       I love coding and I have a great passion about building intuitive UI and dynamic user experience. <br/>
       <span><a href="#contact">Any ideas on mind? Let's connect!</a> 👋</span>
       </p>
-      <div className="contact-icons">
-        <a href="tel:647-515-0170"><i class="fas fa-mobile"></i><span>Phone</span></a>
-        <a href="mailto:pravton_dev@outlook.com"><i class="fas fa-at"></i><span>Email</span></a>
-        <a href="https://github.com/pravton" target="_blank" rel="noreferrer"><i class="fab fa-github"></i><span>Github</span></a>
-        <a href="https://app.codesignal.com/profile/pravton" target="_blank" rel="noreferrer"><i class="fas fa-code"></i><span>Code Signal</span></a>
-        <a href="https://www.linkedin.com/in/pravton/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin-in"></i><span>LinkedIn</span></a>
-      </div>
+      <ContactIcons />
     </section>
   )
 }
